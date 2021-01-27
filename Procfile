40136c5354e16e55ae520791f8402b65151c2ec1
velikiiadmin <73746565+velikiiadmin@users.noreply.github.com>
2021-01-27 12:57:40 +0200

    123

--- Procfile
+++ Procfile
@@ -1,4 +1,2 @@
-pip install gunicorn
 ps:scale web=1
 web: gunicorn myblog.wsgi
-web: python manage.py runserver 0.0.0.0:8000
--- myblog/settings.py
+++ myblog/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'ckeditor',
     'ckeditor_uploader',
+    'django_heroku',
 ]
 
 MIDDLEWARE = [
