75eb741dc6033ed0562289a552d9a53eefbc7794
velikiiadmin <73746565+velikiiadmin@users.noreply.github.com>
2021-01-28 10:14:24 +0200

    Update settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '!@f9u88e9d$0n$eltw&y5_i6q^fu_m(d4=y)_tza2@qt)^h=u+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['dart-blog-v.herokuapp.com']
+ALLOWED_HOSTS = ['dart-blog-v.herokuapp.com', '10.15.186.69', '*']
 
 
 # Application definition
